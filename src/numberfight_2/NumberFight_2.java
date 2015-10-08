664289cfed25ac5d3435f7778feef5e2ccb14c68
vennila <baby.nila05@gmail.com>
2015-10-08 23:43:52 +0100

    "This is another version for handson_2- Initial commit"

---
+++ src/numberfight_2/NumberFight_2.java
@@ -0,0 +1,22 @@
+
+package numberfight_2;
+
+import java.util.Random;
+
+
+public class NumberFight_2 {
+
+    
+    public static void main(String[] args) {
+        int M,B;
+        Random rn = new Random();
+        M = rn.nextInt(10)+1;
+        B = rn.nextInt(10)+1;
+        if (M>B){
+            System.out.println("Micheal won the girl, the value of Micheal is " + M + " and the value of Bruce is " + B);
+        }else {
+            System.out.println("Bruce won the girl, the value of Bruce is " + B + " and the value of Micheal is " + M);
+        }
+    }
+    
+}
